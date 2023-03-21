e0af5bf9dae7a10f641271fb833a20845ecccf37
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2023-03-21 21:55:09 +0900

    기본 앱 완성

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,14 @@
 import { useCounterStore } from "../store";
 
 export default function Counter() {
-  const { count, increment } = useCounterStore();
+  const { count, increment, reset, setNeumber } = useCounterStore();
 
   return (
     <div className="counter">
       <p>{count}</p>
       <button onClick={increment}> + 1 </button>
+      <button onClick={reset}> Reset </button>
+      <button onClick={() => setNeumber(3)}> Setting Number = 3</button>
     </div>
   );
 }
--- src/store.js
+++ src/store.js
@@ -3,4 +3,6 @@ import { create } from "zustand";
 export const useCounterStore = create((set) => ({
   count: 1,
   increment: () => set((state) => ({ count: state.count + 1 })),
+  reset: () => set({ count: 1 }),
+  setNeumber: (number) => set({ count: number }),
 }));
